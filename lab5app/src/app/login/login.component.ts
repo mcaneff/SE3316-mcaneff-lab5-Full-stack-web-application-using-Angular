c1ec749c8bb1338d24978364fc163475d49f8c7c
Michael <mcaneff@uwo.ca>
2018-11-30 01:35:28 +0000

    trying to get the website to run in production mode. Cannot get firebase to host server. Keeps displaying generic O/P ever when source file is chaged

--- lab5app/src/app/login/login.component.ts
+++ lab5app/src/app/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';// imports al fire base functionality-> given alius as firebase 
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private angFireAuth: AngularFireAuth) {
   }
-
+  
+  login(){// the login function for google authentication linking to google framework to do the actual auth 
+    this.angFireAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
 }
